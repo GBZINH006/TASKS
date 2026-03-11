18db30cce84e24bb93ebfdf127ee4152887fb6a0
GBZINH006 <gabrieldossantosdutra06@gmail.com>
2026-03-11 09:52:23 -0300

    app.js, index.js, Task.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const sequelize = require("./database");
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", taskRoutes);
+
+sequelize.sync().then(() => {
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000 🚀")
+    });
+});
